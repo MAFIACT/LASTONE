4af0c6a7fa41d9c0963dad0ef2850fd9105c9697
MAFIACT <gregoire.pitrel@gmail.com>
2025-04-15 01:41:15 +0200

    Update wallet.js

--- src/lib/wallet.js
+++ src/lib/wallet.js
@@ -6,7 +6,7 @@ import { publicProvider } from 'wagmi/providers/public'
 import { EthereumClient } from '@web3modal/ethereum'
 import { Web3Modal } from '@web3modal/react'
 
-export const projectId = 'YOUR_PROJECT_ID' // Replace with your actual WalletConnect project ID
+export const projectId = '050d3abd2409cfc2ab142c17fbd0a77c' // Replace with your actual WalletConnect project ID
 
 const { chains, publicClient } = configureChains([base], [publicProvider()])
 
